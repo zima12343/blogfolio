43e73daf966b060c0692a4f73b7713edbb1ed905
siarhei.Kovalkov <s.kovalkov@lacit.net>
2024-12-28 23:20:02 +0300

    drop

--- client/src/ui_components/Buttons/BurgerButton/BurgerButton.tsx
+++
@@ -1,9 +0,0 @@
-import style from './burgerButton.module.scss'
-
-const BurgerButton = () => {
-    return <div className={style.wrap}>
-        <i className={"fa-solid fa-bars " + style.burger}></i>
-    </div>
-}
-
-export default BurgerButton
--- client/src/ui_components/Buttons/SearchButton/SerchButton.tsx
+++
@@ -1,8 +0,0 @@
-import style from './searchButton.module.scss'
-const SearchButton = () => {
-    return <div className={style.wrap}>
-        <i className={"fa-solid fa-magnifying-glass " + style.searchIcon}></i>
-    </div>
-}
-
-export default SearchButton;
